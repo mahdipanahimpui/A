6e341af46b0bf7602d5923386fe20d7e285b0f85
mahdi <mahdipanahimpui@gmail.com>
2023-07-15 03:11:28 -0400

    models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=120) # max_length is required
+    body = models.TextField()
+    created = models.DateTimeField() 
