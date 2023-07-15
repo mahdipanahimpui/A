fc17ba9aa52d3799eeaa5926a018ca7eb649a932
mahdi <mahdipanahimpui@gmail.com>
2023-07-15 02:49:22 -0400

    var_tags_filters

--- home/views.py
+++ home/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def say_hello(request):
-    return render(request, 'hello.html')
+def say_hello(request): 
+    person = {"name": "mahdi"}
+    return render(request, 'hello.html', context=person)
 
 
 def home(request):
